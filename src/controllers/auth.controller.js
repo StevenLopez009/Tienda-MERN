f104a94ce861983160ba12f5333f6c3d08704b4d
Steven Lopez <stevenlopez009r@gmail.com>
2024-11-22 22:21:00 -0500

    login y register

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,67 @@
+import userModel from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
+
+export const register = async (req, res) => {
+  const { email, password, username } = req.body;
+
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newUser = new userModel({
+      email,
+      password: passwordHash,
+      username,
+    });
+    const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await userModel.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect Password" });
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+  const userFound = await userModel.findById(req.user.id);
+  if (!userFound) return res.status(400).json({ message: "User Not Found" });
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+  });
+};
